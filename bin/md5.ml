9986b2949082611e32d7ad0e31a65cb1623041c8
Alex Hanson <cryptunix@proton.me>
2025-07-05 21:11:46 -0500

    Sequence of arguments :O woah

--- bin/md5.ml
+++ bin/md5.ml
@@ -4,26 +4,27 @@ open Core
 
 (* md5sum is a command-line utility in Unix-like operating systems that implements the MD5 algorithm. *)
 
-let do_hash file =
-  Md5.digest_file_blocking file |> Md5.to_hex |> print_endline
+let get_contents = function
+  | "-" -> In_channel.input_all In_channel.stdin
+  | filename -> In_channel.read_all filename
 
-let regular_file =
-  Command.Arg_type.create (fun filename ->
-    match Sys_unix.is_file filename with
-      | `Yes -> filename
-      | `No -> failwith "Not a regular file"
-      | `Unknown ->
-        failwith "Could not determine if this was a regular file")
+let do_hash filename =
+  get_contents filename
+  |> Md5.digest_string
+  |> Md5.to_hex
+  |> fun md5 -> printf "MD5 (%s) = %s\n" filename md5
 
 let command =
   Command.basic
-    ~summary: "Generate an MD5 hash of the input data lol"
-    ~readme:(fun () -> "This program generates an MD5 hash, which is a unique digital fingerprint, for any given file.
-                        Use it to quickly check if a file has been changed or to confirm its authenticity.")
-    (let%map_open.Command filename =
-       anon ("filename" %: regular_file)
+    ~summary:"Generate an MD5 hash of the input data"
+    ~readme:(fun () -> "More detailed information")
+    (let%map_open.Command files =
+       anon (sequence ("filename" %: Filename_unix.arg_type))
      in
-     fun () -> do_hash filename)
+     fun () ->
+       match files with
+       | [] -> do_hash "-"
+       | _ -> List.iter files ~f:do_hash)
 
 let () =
   Command_unix.run ~version:"1.0" ~build_info:"RWO" command
